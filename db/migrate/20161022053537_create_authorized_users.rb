45bb9f50e2a70c0829c6f62f713cec88d0d15391
Jiacheng Wu <daisyyvonnewu@gmail.com>
2016-10-22 05:41:15 +0000

    Fix migration issues

---
+++ db/migrate/20161022053537_create_authorized_users.rb
@@ -0,0 +1,10 @@
+class CreateAuthorizedUsers < ActiveRecord::Migration
+  def change
+    create_table :authorized_users, :force => true  do |t|
+      t.string :email                  # default: "",      null: false
+      t.string :role                   # default: "coach", null: false
+    end
+    add_index :authorized_users, :email
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021113555) do
+ActiveRecord::Schema.define(version: 20161022053828) do
 
   create_table "authorized_users", force: :cascade do |t|
     t.string "email"
