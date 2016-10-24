ec692724f2af188557d533caa59daada2ad26420
Jiacheng Wu <daisyyvonnewu@gmail.com>
2016-10-24 19:41:07 +0000

    Fix issues in migration file

--- db/migrate/20161021113555_create_normal_user.rb
+++ db/migrate/20161021113555_create_normal_user.rb
@@ -1,9 +1,9 @@
 class CreateNormalUser < ActiveRecord::Migration
-  def self.up
+  def up
   	User.create!(email: "test@berkeley.edu", password: "asdf1234", role: "coach")
   end
 
-  def self.down
+  def down
   	user = User.find_by_email("test@berkeley.edu")
   	user.destroy
   end
