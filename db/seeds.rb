8e9f7a36ff2cf9d95513b9f372dc7cb25559443e
DrakeW <junyuw@berkeley.edu>
2016-11-14 13:25:39 -0800

    updated seeds to generate data across timeline

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,58 +8,53 @@
 require 'project_metric_code_climate' 
 require 'project_metric_slack_trends'
 require 'project_metric_pivotal_tracker'
-dummy1_code_climate = ProjectMetrics.class_for('code_climate').new url: 'http://github.com/AgileVentures/WebsiteOne'
-dummy1_github = ProjectMetrics.class_for('github').new url: 'http://github.com/AgileVentures/WebsiteOne'
-dummy1_slack = ProjectMetrics.class_for('slack').new(token: 'xoxp-88866518725-89002779974-99405676995-ce5416e3518e3738525b6290720418e1', channel:'general')
-dummy1_pivot = ProjectMetrics.class_for('pivotal_tracker').new(token: 'd9ef66309fcefb28ccb33863a922f8f5', project:'1886749')
-dummy1_slack_trends = ProjectMetrics.class_for('slack_trends').new(token: 'xoxp-88866518725-89002779974-99405676995-ce5416e3518e3738525b6290720418e1', channel:'general')
 
+dummy1_code_climate = ProjectMetrics.class_for('code_climate').new url: 'http://github.com/AgileVentures/WebsiteOne'
 dummy2_code_climate = ProjectMetrics.class_for('code_climate').new url: 'http://github.com/AgileVentures/project_metric_slack'
-dummy2_github = ProjectMetrics.class_for('github').new url: 'http://github.com/AgileVentures/project_metric_slack'
-slack1 = File.read './db/image/slack1.svg'
-slack2 = File.read './db/image/slack2.svg'
-slack3 = File.read './db/image/slack3.svg'
-slack_trends1 = File.read './db/image/slack_trends1.svg'
-slack_trends2 = File.read './db/image/slack_trends2.svg'
-slack_trends3 = File.read './db/image/slack_trends3.svg'
-pivot1 = File.read './db/image/pivot1.svg'
-pivot2 = File.read './db/image/pivot2.svg'
-github1 = File.read './db/image/github1.svg'
-github2 = File.read './db/image/github2.svg'
-github3 = File.read './db/image/github3.svg'
-Project.create!(:name => "THE ARCTIC INSTITUTE", 
-    :configs => [
-        Config.create!(:metric_name => 'code_climate', :project_id => 1, :options => {'url' => 'http://github.com/AgileVentures/WebsiteOne'}),
-        Config.create!(:metric_name => 'github', :project_id => 1, :options => {'url' => 'http://github.com/AgileVentures/WebsiteOne'}),
-        Config.create!(:metric_name => 'slack', :project_id => 1, :options => {'token' => 'xoxp-88866518725-89002779974-99405676995-ce5416e3518e3738525b6290720418e1', 'channel'=>'general'}),
-        Config.create!(:metric_name => 'pivotal_tracker', :project_id => 1, :options => {'token' => 'd9ef66309fcefb28ccb33863a922f8f5', 'project'=>'1886749'}),
-        Config.create!(:metric_name => 'slack_trends', :project_id => 1, :options => {'token' => 'xoxp-88866518725-89002779974-99405676995-ce5416e3518e3738525b6290720418e1', 'channel'=>'general'})
-        ], 
-    :metric_samples =>[
-        MetricSample.create!(:metric_name => 'code_climate', :project_id => 1, :score => dummy1_code_climate.score, :raw_data => dummy1_code_climate.raw_data, :image => dummy1_code_climate.raw_data), 
-        MetricSample.create!(:metric_name => 'github', :project_id => 1, :score => dummy1_github.score, :raw_data => dummy1_github.raw_data, :image => dummy1_github.image), 
-        MetricSample.create!(:metric_name => 'slack', :project_id => 1, :score => dummy1_slack.score, :raw_data => dummy1_slack.raw_data, :image => dummy1_slack.image),
-        MetricSample.create!(:metric_name => 'pivotal_tracker', :project_id => 1, :score =>dummy1_pivot.score, :image => dummy1_pivot.image),
-        MetricSample.create!(:metric_name => 'slack_trends', :project_id => 1, :score => dummy1_slack_trends.score, :raw_data => dummy1_slack_trends.raw_data, :image => dummy1_slack_trends.image)])
-Project.create!(:name => "ALZHEIMER'S GREATER LOS ANGELES", :metric_samples =>[
-    MetricSample.create!(:metric_name => 'code_climate', :project_id => 2, :score => dummy2_code_climate .score, :raw_data => dummy2_code_climate .raw_data, :image =>dummy2_code_climate.raw_data), 
-    MetricSample.create!(:metric_name => 'github', :project_id => 2, :score => 0.6339563862928349, :image => github1),
-    MetricSample.create!(:metric_name => 'slack', :project_id => 2, :score => 0.29861111111111116, :image => slack1),
-    MetricSample.create!(:metric_name => 'pivotal_tracker', :project_id => 2, :score => 0.5636833046471601, :image => pivot1),
-    MetricSample.create!(:metric_name => 'slack_trends', :project_id => 2, :score => 0.32200793650793647, :image => slack_trends1)])
-Project.create!(:name => "VISIONARIA NETWORK", :metric_samples =>[
-    MetricSample.create!(:metric_name => 'code_climate', :project_id => 3, :score => dummy1_code_climate.score, :image => dummy1_code_climate.raw_data), 
-    MetricSample.create!(:metric_name => 'github', :project_id => 3, :score => 0.6166666666666667, :image => github2), 
-    MetricSample.create!(:metric_name => 'slack', :project_id => 3, :score => 0.25, :image => slack2),
-    MetricSample.create!(:metric_name => 'pivotal_tracker', :project_id => 3, :score => 0.49702380952380953, :image => pivot2),
-    MetricSample.create!(:metric_name => 'slack_trends', :project_id => 3, :score => 0.42148777348777344, :image => slack_trends2)])
-Project.create!(:name => "QuestionBank",
-    :configs => [
-        Config.create!(:metric_name => 'code_climate', :project_id => 4, :options => {'token' => 'xyz', 'user' => 'fox'}),
-        Config.create!(:metric_name => 'github', :project_id => 4, :options => {'token' =>'123', 'user'=>"fox"})], 
-    :metric_samples =>[
-        MetricSample.create!(:metric_name => 'code_climate', :project_id => 4,  :score => dummy2_code_climate .score, :image => dummy2_code_climate.raw_data), 
-        MetricSample.create!(:metric_name => 'github', :project_id => 4, :score => 0.29, :image => github3), 
-        MetricSample.create!(:metric_name => 'slack', :project_id => 4, :score => 0.6, :image => slack3),
-        MetricSample.create!(:metric_name => 'pivotal_tracker', :project_id => 4, :score => 0),
-        MetricSample.create!(:metric_name => 'slack_trends', :project_id => 4, :score => 0.4637265745007681, :image => slack_trends3)])
+
+# generate images
+dummy1_code_climate.image
+dummy2_code_climate.image
+
+slack1 = File.read './db/images/slack1.svg'
+slack2 = File.read './db/images/slack2.svg'
+slack3 = File.read './db/images/slack3.svg'
+slack_trends1 = File.read './db/images/slack_trends1.svg'
+slack_trends2 = File.read './db/images/slack_trends2.svg'
+slack_trends3 = File.read './db/images/slack_trends3.svg'
+pivot1 = File.read './db/images/pivot1.svg'
+pivot2 = File.read './db/images/pivot2.svg'
+github1 = File.read './db/images/github1.svg'
+github2 = File.read './db/images/github2.svg'
+github3 = File.read './db/images/github3.svg'
+
+
+dummies = Hash.new
+dummies["code_climate"] = [dummy1_code_climate.raw_data, dummy2_code_climate.raw_data, dummy1_code_climate.raw_data]
+dummies["github"] = [github1, github2, github3]
+dummies["slack"] = [slack1, slack2, slack3]
+dummies["pivotal_tracker"] = [pivot1, pivot2, pivot2]
+dummies["slack_trends"] = [slack_trends1, slack_trends2, slack_trends3]
+
+
+project_1 = Project.create!(:name => "THE ARCTIC INSTITUTE")
+project_2 = Project.create!(:name => "ALZHEIMER'S GREATER LOS ANGELES")
+project_3 = Project.create!(:name => "VISIONARIA NETWORK")
+project_4 = Project.create!(:name => "QuestionBank")
+
+projects_list = [project_1, project_2, project_3, project_4]
+
+
+start_date = Date.parse("2016-11-6")
+end_date = Date.parse("2016-11-14")
+
+start_date.upto(end_date) do |date|
+    projects_list.each do |project|
+        ProjectMetrics.metric_names.each do |metric|
+            MetricSample.create!(:metric_name => metric,
+                                 :project_id => project.id,
+                                 :score => rand(4),
+                                 :image => dummies[metric][rand(3)])
+        end
+    end
+end
