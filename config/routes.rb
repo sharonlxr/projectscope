e1a55bace5f06bd7973b5fd07227d69305d6728d
DrakeW <junyuw@berkeley.edu>
2016-11-28 00:50:15 -0800

    converted indentation to 2 spaces

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:password]
   
   resources :projects do
-  	collection do
-  		post "/metrics_on_date", :to => "projects#metrics_on_date"
-  	end
+    collection do
+      post "/metrics_on_date", :to => "projects#metrics_on_date"
+    end
   end
   root 'projects#index'
 end
--- spec/javascripts/project_spec.js
+++ spec/javascripts/project_spec.js
@@ -1,14 +1,14 @@
 describe("sliding timeslider", function() {
-	it("should make an AJAX request to the correct URL", function() {
-		var doneCallback = {
-			done: function() {}
-		};
-		var failCallback = {
-			fail: function() {}
-		};
-		spyOn($, "ajax").and.returnValue(doneCallback);
-		spyOn(doneCallback, 'done').and.returnValue(failCallback);
-		request_for_metrics(-1);
-		expect($.ajax.calls.mostRecent().args[0]["url"]).toEqual("projects/metrics_on_date");
-	});
+  it("should make an AJAX request to the correct URL", function() {
+    var doneCallback = {
+      done: function() {}
+    };
+    var failCallback = {
+      fail: function() {}
+    };
+    spyOn($, "ajax").and.returnValue(doneCallback);
+    spyOn(doneCallback, 'done').and.returnValue(failCallback);
+    request_for_metrics(-1);
+    expect($.ajax.calls.mostRecent().args[0]["url"]).toEqual("projects/metrics_on_date");
+  });
 });
