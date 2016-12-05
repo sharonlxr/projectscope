b714f54c08ae9aa9e6355538a595ef26ce1ea210
DrakeW <junyuw@berkeley.edu>
2016-12-05 00:29:44 -0800

    codeclimate

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,19 +3,19 @@ var ready = function() {
 		var project_checkboxes = $('.project_checkbox');
 		var metric_checkboxes = $('.metric_checkbox');
 		if (this.checked) {
-			$.each(project_checkboxes, function(index, el) {
-				if (el.checked !== true) {
-					el.click();
-				}
-			});
-			$.each(metric_checkboxes, function(index, el) {
-				if (el.checked !== true) {
-					el.click();
-				}
-			});
+			check_all(project_checkboxes);
+			check_all(metric_checkboxes);
 		}
 	});
 };
+
+var check_all = function(checkboxes) {
+	$.each(checkboxes, function(index, el) {
+		if (el.checked !== true) {
+			el.click();
+		}
+	});
+}
 
 $(document).on('turbolinks:load', ready);
 $(document).ready(ready);
