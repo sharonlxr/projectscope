47c9c5de1c5fe6c7605cc439468fb0800f024872
yanxinyi <yanxinyi@gmail.com>
2017-04-13 15:26:43 -0700

    add config value to seed

--- app/models/config.rb
+++ app/models/config.rb
@@ -14,15 +14,15 @@
 class Config < ActiveRecord::Base
   belongs_to :project
   scope :for, ->(metric_name) { where(:metric_name => metric_name).first }
-  validates_uniqueness_of :metric_name, :scope => :project_id
-
-  attr_encrypted :options, :key => Figaro.env.attr_encrypted_key!, :marshal => true
-  after_initialize { self.options ||= {} }
-
-  attr_accessible :options, :metric_name
-
-  def klass
-  	ProjectMetrics.class_for self.metric_name
-  end
+  # validates_uniqueness_of :metric_name, :scope => :project_id
+  #
+  # attr_encrypted :options, :key => Figaro.env.attr_encrypted_key!, :marshal => true
+  # after_initialize { self.options ||= {} }
+  #
+  # attr_accessible :options, :metric_name
+  #
+  # def klass
+  # 	ProjectMetrics.class_for self.metric_name
+  # end
 end
 
