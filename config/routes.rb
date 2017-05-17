9d60430a9cae8d5e573103824298ba556dfdb302
An Ju <an_ju@berkeley.edu>
2017-05-17 12:48:09 -0700

    Refactor to remove some unnecessary routes and make routes.rb looks better. Change views according to user roles. Change seed data and login page to test different roles.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   resources :comments
   resources :users, :only => [:show, :update], :path => "user"
   resources :projects do
-    collection do
-      post "/metrics_on_date", :to => "projects#metrics_on_date"
-    end
   	member do
   		post "/add_owner", :to => "projects#add_owner"
       get '/metrics/:metric', :to => 'projects#get_metric_data'
       get '/metrics/:metric/detail', to: 'projects#show_metric'
+      get '/new_edit', :to => 'projects#new_edit', :as => 'new_edit_project'
+      post '/new_update', :to => 'projects#new_update', :as => 'update_metric'
   	end
   end
   resources :whitelists do
@@ -23,14 +22,9 @@ Rails.application.routes.draw do
       :to => 'comments#comments_for_metric_sample',
       :as => 'metric_sample_comments'
 
-  # get '/application/update_all_projects/', :to => 'application#update_all_projects', :as => 'update_all_projects'
-
   get '/login/:id', :to => 'application#passthru', :as => 'passthru'
 
   get '/dev', :to => 'projects#new_index', :as => 'new_index'
-  get 'all_metrics/:id', :to => 'projects#all_metrics', :as => 'all_metrics'
-  get '/charts', :to => 'projects#charts'
-  get 'projects/:id/new_edit', :to => 'projects#new_edit', :as => "new_edit_project"
-  post 'projects/:id/new_update', :to => 'projects#new_update', :as => "update_metric"
+
   root 'projects#new_index'
 end
