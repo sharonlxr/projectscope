f7d939e0a421e04050c8c435e5244cab04314535
An Ju <an_ju@berkeley.edu>
2017-05-22 14:59:55 -0700

    Add placeholder for projects that have not yet been graded.

--- app/assets/javascripts/gauge.js
+++ app/assets/javascripts/gauge.js
@@ -4,9 +4,17 @@
 
 function gauge(containerID, data) {
     var color = d3.scaleLinear().domain([0.0, 2.0, 4.0]).range(['red', 'yellow', 'green']);
-    d3.select('#' + containerID)
-        .style('background-color', color(data.data.score))
-        .style('width', (100.0 * data.data.score / 4.0) + '%')
-        .style('float', 'left')
-        .html('GPA: ' + data.data.score);
+    if (data.data.score === 0.0) {
+        d3.select('#' + containerID)
+            .style('background-color', 'gray')
+            .style('width', '100%')
+            .style('float', 'left')
+            .html('Not Graded.');
+    } else {
+        d3.select('#' + containerID)
+            .style('background-color', color(data.data.score))
+            .style('width', (100.0 * data.data.score / 4.0) + '%')
+            .style('float', 'left')
+            .html('GPA: ' + data.data.score);
+    }
 }
