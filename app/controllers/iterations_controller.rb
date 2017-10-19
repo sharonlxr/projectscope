fea0c5f2530108885d09ea030b18f0db8198d979
sharon li <sharonlxr@berkeley.edu>
2017-10-19 21:10:26 +0000

    fix ,

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -13,7 +13,8 @@ class IterationsController < ApplicationController
   def edit
 
     @iteration = Iteration.find(params[:id])
-    @tasks = Task.joins(:iteration).where('iteration_id': params[:id])
+    @tasks = Task.where('iteration_id': params[:id])
+    # @tasks = Task.joins(:iteration).where('iteration_id': params[:id])
     @tasks.each do|e|
       puts e.title
       puts e.iteration_id
