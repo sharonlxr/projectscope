ed0944dc3c46d587dc575340536dfa6184950cc3
sharon li <sharonlxr@berkeley.edu>
2017-10-19 06:10:11 +0000

    fix some bugs

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,28 +2,36 @@ class TaskController < ApplicationController
     #load the form for new task
     def new
         #get the iteration going to be added to
-        @iteration = params[:iter]
+        @iteration = Task.find(params[:iter])
         #get the list of current tasks in the iteration selected
-        @tasks = Task.where("iteration_id",@iteration.id).uniq.pluck(:title)
+        @all_tasks = Task.where("iteration_id",@iteration.id).uniq.pluck(:title)
+        if !@all_tasks
+            @all_tasks =[]
+        end
     end
     #create new tasks
     def create
-        @iteration = params[:iter]
+        @iteration = Task.find(params[:iter])
         @tasks = Task.where("iteration_id",@iteration.id)
+        if !@tasks
+            @tasks =[]
+        end
         parants_param = params[:tasks]
         task_param = params[:task]
         new_task = Task.new
-        new_task.title=task_param.title
-        new_task.description=task_param.description
+        new_task.title=task_param[:title]
+        new_task.description=task_param[:description]
         new_task.save
         #added all the checked tasks to parents
+       
         @tasks.each do |p|
-            if parants_param[p.title]=="true"
+            
+            if p.title and parants_param[p.title]=="true"
                 new_task.add_parent(p)
             end
         end
         ##need to add display message and direct to some page 
         flash[:message]= ""
-        
+        redirect_to iterations_path
     end
 end
--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -25,7 +25,13 @@ Given(/^I select "([^"]*)" and press edit$/) do |task|
 end
 
 Given (/^I create Task "([^"]*)" to "([^"]*)"$/) do |title, description|
-  pending
+  iter = Iteration.find_by_name("iteration_1")
+  t1 = Task.new
+  t1.iteration=iter
+  t1.title=title
+  t1.description=description
+  t1.save
+  # pending
 end
 
 # And (/^I  am on Iteration "([^"]*)"$/) do |iter|
