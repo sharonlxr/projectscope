e0aec1e5cf50749b8e6119bbf14b19763f8fa541
Luke Larson <luke.larson@berkeley.edu>
2017-10-31 04:08:59 +0000

    got student commenting working, but rn they can comment on any project, not just their own

--- features/step_definitions/student_comment_steps.rb
+++ features/step_definitions/student_comment_steps.rb
@@ -1,15 +1,29 @@
 Given(/the following users exist:/) do |users_table|
   users_table.hashes.each do |user|
-    user["password"] = Devise.friendly_token[0,20]
-    user["preferred_metrics"] = []
-    user["preferred_projects"] = []
-    user["project"] = Project.find user["project"].to_i
-    if user["role"] == "student"
-      user["role"] = User::STUDENT
-    elsif user["role"] == "admin"
-      user["role"] = User::ADMIN
-    end
-    User.create user
+    
+    u = User.create!(password: Devise.friendly_token[0,20],
+                 preferred_metrics: [],
+                 preferred_projects: [],
+                 project: Project.find_by(name: user["project"]),
+                 role: user_type_from_string(user["role"]),
+                 provider_username: user["provider_username"],
+                 uid: user["uid"],
+                 email: user["email"],
+                 provider: user["provider"]
+                 )
+    puts(u.project.id)
+    puts(MetricSample.find_by(metric_name: "code_climate").project_id)
+  end
+end
+
+#helper for creating users
+def user_type_from_string(string)
+  if string == "student"
+    return User::STUDENT
+  elsif string == "admin"
+    return User::ADMIN
+  else
+    raise "user_type_from_string requires 'student' or 'admin' got: '#{string}'"
   end
 end
 
@@ -28,24 +42,39 @@ Given(/the following metrics exist:/) do |metrics_table|
 
 end
 
-Given(/I am a "(.*)" logged in/) do |type|
-  num = User.count
-  if type == "student"
-    student_user = User.create!(provider_username: "Student#{num}", uid: "ustudent#{num}", email: "ustudent#{num}@example.com",
-                              provider: "developer", role: User::STUDENT, password: Devise.friendly_token[0,20])
-    ENV['STUDENT_PASSWORD'] = 'password'
-    visit "/login/#{student_user.uid}?passwd=password"
-  elsif type == "admin"
-    admin_user = User.create!(provider_username: "Admin#{num}", uid: "uadmin#{num}", email: "uadmin#{num}@example.com",
-                              provider: "developer", role: User::ADMIN, password: Devise.friendly_token[0,20])
+Given(/I am "(.*)" and logged in/) do |name|
+  
+  user = User.find_by(uid: name)
+  if user.role == "student"
     ENV['ADMIN_PASSWORD'] = 'password'
-    visit "/login/#{admin_user.uid}?passwd=password"
+    visit "/login/#{user.uid}?passwd=password"
+    
+  elsif user.role == "admin"
+    ENV['ADMIN_PASSWORD'] = 'password'
+    visit "/login/#{user.uid}?passwd=password"
+    
+  #elsif type == "admin"
+  #  admin_user = User.create!(provider_username: "Admin#{num}", uid: "uadmin#{num}", email: "uadmin#{num}@example.com",
+  #                            provider: "developer", role: User::ADMIN, password: Devise.friendly_token[0,20])
+  #  ENV['ADMIN_PASSWORD'] = 'password'
+  #  visit "/login/#{admin_user.uid}?passwd=password"
+    
   else
-    raise "type must be 'student' or 'admin', got '#{type}'"
+    raise "type must be 'student' or 'admin', got '#{name}'"
   end
 end
 
-Given(/there is a "(.*)" comment "(.*)" on project "(.*)" metric "(.*)"/) do |user_type, comment, project, metric|
-    pending
+#Since comments are tied to metric samples, not metrics. We will take this comment to be on the most reacent sample of this metric on this project
+Given(/there is a "(.*)" comment "(.*)" on project "(.*)" metric "(.*)"/) do |user, comment, project, metric|
+    require 'date'
+    project = Project.find_by(name: project)
+    metric_sample = project.latest_metric_sample(metric)
+    this_user = User.find_by(uid: user).id
+    Comment.create!(metric_sample_id: metric_sample.id, 
+                    user_id: this_user,
+                    ctype: 'general_comment',
+                    params: '{}',
+                    created_at: Date.today,
+                    content: comment)
 end
 
--- features/student_comment.feature
+++ features/student_comment.feature
@@ -16,30 +16,41 @@ Background: users and project in the data base
   | github       | Project_1 | 3     |
 
   Given the following users exist:
-  | provider_username       | uid         | email               | provider  | role    | preferred_metrics | project |
-  | Admin                   | uadmin      | uadmin@example.com  | developer | admin   | []                | 1       |
-  | Student                 | ustudent    | ustudent@example.com| developer | student | []                | 1       |
-  
+  | provider_username       | uid         | email               | provider  | role    | project   |
+  | Admin                   | uadmin      | uadmin@example.com  | developer | admin   | Project_1 |
+  | Student                 | ustudent    | ustudent@example.com| developer | student | Project_1 |
+
+#this feature is alrady implimented, so the test is a sanity check on testing apparatus
+Scenario: add new comment as admin
+  Given I am "uadmin" and logged in
+  And I am on the "view project 'Project_1'" page
+  And I follow "Code Climate"
+  Then I should see "Submit"
+  And I fill in "content" with "this is an admin comment"
+  And I press "Submit"
+  And I follow "Project_1"
+  Then I should see "this is an admin comment"
+
+Scenario: add new comment as student
+  Given I am "ustudent" and logged in
+  And I am on the "view project 'Project_1'" page
+  And I follow "Code Climate"
+  Then I should see "Submit"
+  And I fill in "content" with "this is a student comment"
+  And I press "Submit"
+  #if user is a student, we are redirected from projects#index to our users project page, so we don't need to click on the link 
+  #like we did for the admin 
+  Then I should see "this is a student comment"
 
 Scenario: see reply to metric comment as student
-  Given I am a "student" logged in
-  And there is a "admin" comment "ad com" on project "Project_1" metric "metric_1"
+  Given I am "ustudent" and logged in
+  And there is a "uadmin" comment "ad com" on project "Project_1" metric "code_climate"
   And I am on the "view project 'Project_1'" page
   Then I should see "reply"
   
-Scenario: add new comment as student
-  Given I am a "student" logged in
-  And I am on the "view project 'Project_1'" page
-  And I follow "Code Climate"
-  Then I should see "comment"
-  And I fill in "comment" with "this is a student comment"
-  And I press "submit"
-  And I follow "Project_1"
-  Then I should see "this is a student comment"
-  
 Scenario: reply to a comment as a student
-  Given I am a "student" logged in
-  And there is a "admin" comment "ad com" on project "Project_1" metric "metric_1"
+  Given I am "ustudent" and logged in
+  And there is a "uadmin" comment "ad com" on project "Project_1" metric "code_climate"
   And I am on the "view project 'Project_1'" page
   Then I should see "ad com"
   And I should see "reply"
