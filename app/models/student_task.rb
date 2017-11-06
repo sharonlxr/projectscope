51d451be923ea82ae503e92d97d830fe18e66ac5
sharon li <sharonlxr@berkeley.edu>
2017-11-06 04:31:13 +0000

    [iter3] disable button to update status if the task is blocked

--- app/models/student_task.rb
+++ app/models/student_task.rb
@@ -9,4 +9,17 @@ class StudentTask < ActiveRecord::Base
      def get_parents
         self.parents.map {|p| "\"#{p.title}\""}.join(',')
     end
+    def is_blocked
+        if self.parents.empty?
+            return false
+        else
+            self.parents.each do |p|
+                if p.status != "Finished"
+                    return true
+                end
+            end
+            return false
+        end
+        
+    end
 end
